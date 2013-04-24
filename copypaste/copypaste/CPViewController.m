56abdd143168a6488629a41e056d6072a46cb100
Elliot Lee <elliot.lee@gmail.com>
2013-04-24 13:58:42 -0700

    new paste button

--- copypaste/copypaste/CPViewController.m
+++ copypaste/copypaste/CPViewController.m
@@ -21,12 +21,9 @@
 #define kUserAvatarHeight 60
 #define kUserNameWidth 102
 #define kUserNameHeight 20
-#define kPasteWidth 102
-#define kPasteHeight 48
 
 #define kContentViewTag 777
 #define kLabelViewTag 778
-#define kPasteLabelViewTag 779
 
 #define kOffset 6
 #define kHeaderViewHeight 52
@@ -223,6 +220,28 @@
     return CGSizeMake(kUserHolderWidth, kUserHolderHeight);
 }
 
+- (void)pasteToUserAtPosition:(NSInteger)position
+{
+    NSObject *itemToPaste = [[DataManager sharedManager] getThingsFromClipboard];
+    GSSUser * user = [[[DataManager sharedManager] nearByUserList] objectAtIndex:position];
+    if (itemToPaste) {
+        [[GSSSession activeSession] sendMessage:itemToPaste toUser:user];
+        
+    } else {
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Can not paste"
+                                                            message:[NSString stringWithFormat:@"Your clipboard is empty, please copy something to paste to %@!", user.fullname]
+                                                           delegate:nil
+                                                  cancelButtonTitle:@"OK"
+                                                  otherButtonTitles:nil];
+        [alertView show];
+    }
+}
+
+- (void)pasteToUserWithButton:(UIButton *)button
+{
+    [self pasteToUserAtPosition:button.tag];
+}
+
 - (GMGridViewCell *)GMGridView:(GMGridView *)gridView_ cellForItemAtIndex:(NSInteger)index {
     GMGridViewCell *cell = [gridView_ dequeueReusableCellWithIdentifier:@"layerCell"];
     
@@ -259,14 +278,14 @@
         contentLabel.tag = kLabelViewTag;
         [cell addSubview:contentLabel];
         
-        UIImageView *pasteImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0,
-                                                                                    kUserHolderHeight - kPasteHeight,
-                                                                                    kPasteWidth,
-                                                                                    kPasteHeight)];
-        pasteImageView.backgroundColor = [UIColor clearColor];
-        pasteImageView.tag = kPasteLabelViewTag;
-        pasteImageView.image = [UIImage imageNamed:@"paste.png"];
-        [cell addSubview:pasteImageView];
+        UIImage *pasteButtonImage = [UIImage imageNamed:@"pastebutton.png"];
+        UIButton *pasteButton = [[UIButton alloc] initWithFrame:CGRectMake(0, kUserHolderHeight - pasteButtonImage.size.height,
+                                                                           pasteButtonImage.size.width, pasteButtonImage.size.height)];
+        pasteButton.backgroundColor = [UIColor clearColor];
+        [pasteButton setImage:pasteButtonImage forState:UIControlStateNormal];
+        pasteButton.tag = index; // tag is used by -pasteToUserWithButton: to identify the user position
+        [pasteButton addTarget:self action:@selector(pasteToUserWithButton:) forControlEvents:UIControlEventTouchUpInside];
+        [cell addSubview:pasteButton];
     }
     
     if ([[[DataManager sharedManager] nearByUserList] count] == 0) {
@@ -293,20 +312,9 @@
     return cell;
 }
 
-- (void)GMGridView:(GMGridView *)gridView_ didTapOnItemAtIndex:(NSInteger)position {
-    NSObject *itemToPaste = [[DataManager sharedManager] getThingsFromClipboard];
-    GSSUser * user = [[[DataManager sharedManager] nearByUserList] objectAtIndex:position];
-    if (itemToPaste) {
-        [[GSSSession activeSession] sendMessage:itemToPaste toUser:user];
-        
-    } else {
-        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Can not paste"
-                                                            message:[NSString stringWithFormat:@"Your clipboard is empty, please copy something to paste to %@!", user.fullname]
-                                                           delegate:nil
-                                                  cancelButtonTitle:@"OK"
-                                                  otherButtonTitles:nil];
-        [alertView show];
-    }
+- (void)GMGridView:(GMGridView *)gridView_ didTapOnItemAtIndex:(NSInteger)position
+{
+    [self pasteToUserAtPosition:position];
 }
 
 - (void)imageViewLoadedImage:(EGOImageView *)imageView {
