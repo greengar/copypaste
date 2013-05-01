988f129ad7d086351d71430cba09286f451fc9ce
Hector Zhao <long@greengar.com>
2013-04-30 17:03:58 -0700

    fix build error with Smartboard framework

---
+++ copypaste/copypaste/SDKs/Smartboard.framework/Versions/1/Headers/GSSignUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  CPSignUpViewController.h
+//  copypaste
+//
+//  Created by Elliot Lee on 4/27/13.
+//  Copyright (c) 2013 Greengar. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+
+@interface GSSignUpViewController : PFSignUpViewController
+
+@end
