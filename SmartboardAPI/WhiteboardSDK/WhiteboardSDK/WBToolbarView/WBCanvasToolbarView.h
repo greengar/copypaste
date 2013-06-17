b6689cc10961e6970a3a09364af72c7af2d8c29e
Elliot Lee <elliot@greengar.com>
2013-06-17 03:53:20 -0700

    Start adding UI
    Not functional yet

---
+++ SmartboardAPI/WhiteboardSDK/WhiteboardSDK/WBToolbarView/WBCanvasToolbarView.h
@@ -0,0 +1,13 @@
+//
+//  WBCanvasToolbarView.h
+//  Whiteboard7
+//
+//  Created by Elliot Lee on 6/12/13.
+//  Copyright (c) 2013 GreenGar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WBCanvasToolbarView : UIView
+
+@end
